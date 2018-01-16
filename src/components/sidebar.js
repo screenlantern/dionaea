cfb1cf024288ca6537f9c62abb651f89edb0fab5
screenlantern <screenlantern@gmail.com>
2018-01-16 01:05:42 +0000

    routing correctly

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Drawer, Navigation } from 'react-mdl';
 
 const SideBar = (props) => {
     return(
         <Drawer className="dashboard__sidebar" title="">
             <Navigation>
-                <a href="/">Attacks Per Country</a>
-                <a href="/protocols">Connections Per Protocol</a>
+                <Link to="/">Attacks Per Country</Link>
+                <Link to="/protocols">Connections Per Protocol</Link>
             </Navigation>
         </Drawer>
     );
